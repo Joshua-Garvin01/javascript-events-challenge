b4edea924c92c7305af452207847e44579024a55
Joshua Garvin <jgarvin6@cnm.edu>
2021-11-01 19:30:25 -0500

    cleaned up, next I'll add final button

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-function changeTextColor() {
-    let PH1 = document.getElementById("PH1")
-    if (PH1.style.color === "black") {
-        PH1.style.color = "red"
-
-        //console.log("turning red")
-    } else {
-            PH1.style.color = "black"
-            // console.log("turning black")
-        }
-}
-
-function changeWords() {
-    let mySentence = document.getElementById("discoTime").innerHTML;
-    // mySentence.replace("jazz", "disco");
-    // This replaces multiple words
-    mySentence.replace(/Jazz/g, "disco");
-    document.getElementById("discoTime").innerHTML = mySentence;
-}
-
 const handleButtonColorChange = () => {
     let button = document.getElementById('target')
     //button.style.backgroundColor = "red"
@@ -28,3 +8,24 @@ const handleButtonColorChange = () => {
     }
 }
 
+function changeTextColor() {
+    let PH1 = document.getElementById("PH1")
+    if (PH1.style.color === "black") {
+        PH1.style.color = "red"
+
+    } else {
+            PH1.style.color = "black"
+
+        }
+}
+
+function changeTheWords() {
+    let mySentence = document.getElementById("discoTime").innerHTML;
+
+    let newSentence = mySentence.replace(/jazz/gi, "DISCO-TIME!!!");
+
+    document.getElementById("discoTime").innerHTML = newSentence;
+}
+
+function changeOneWord() {
+}
